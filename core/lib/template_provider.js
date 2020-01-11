35e8d43701612a21be73d788094f6502d878fe61
Mitchell Stokes <M.Stokes@F5.com>
2020-01-10 16:05:08 -0800

    core/template_provider: Remove unused GitHub and Postman providers
    We can bring these back if/when we want them again.

--- core/lib/template_provider.js
+++ core/lib/template_provider.js
@@ -6,7 +6,6 @@ const ioUtil = require('./io_util.js');
 const Template = require('./template').Template;
 
 const ResourceCache = ioUtil.ResourceCache;
-const makeRequest = ioUtil.makeRequest;
 
 function FsTemplateProvider(templateRootPath, schemaProvider) {
     this.config_template_path = templateRootPath;
@@ -42,7 +41,6 @@ FsTemplateProvider.prototype.fetch = function fetch(key) {
     return this.cache.fetch(key);
 };
 
-// used for listing AS3 templates available
 FsTemplateProvider.prototype.list = function list() {
     return new Promise((resolve, reject) => {
         fs.readdir(this.config_template_path, (err, data) => {
@@ -61,54 +59,6 @@ FsTemplateProvider.prototype.list = function list() {
     }));
 };
 
-function GitHubTemplateProvider(githubRepoPath) {
-    this.githubRepoPath = githubRepoPath;
-
-    this.list_opts = {
-        host: 'api.github.com',
-        path: `/repos/${githubRepoPath}/contents/templates/simple`,
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
-        }
-    };
-
-    this.fetch_opts = {
-        host: 'raw.githubusercontent.com',
-        path: 'path/to/directory/contents',
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
-        }
-    };
-
-    this.cache = new ResourceCache((templateName) => {
-        this.fetch_opts.path = `/${this.githubRepoPath}/master/templates/simple/${templateName}.mst`;
-        return makeRequest(this.fetch_opts).then(result => result.body);
-    });
-
-    return this;
-}
-
-GitHubTemplateProvider.prototype.fetch = function fetch(templateName) {
-    return this.cache.fetch(templateName);
-};
-
-GitHubTemplateProvider.prototype.list = function list() {
-    return makeRequest(this.list_opts).then((result) => {
-        if (result.status === '404') {
-            throw new Error(`Repository not found: ${this.githubRepoPath}`);
-        }
-        return JSON.parse(result.body)
-            .map(fileMeta => fileMeta.name.split('.')[0]);
-    });
-};
-
-function PostmanTemplateProvider(postmanCollection) {
-    this.collection = postmanCollection;
-    return this;
-}
-
 module.exports = {
-    FsTemplateProvider,
-    GitHubTemplateProvider,
-    PostmanTemplateProvider
+    FsTemplateProvider
 };
