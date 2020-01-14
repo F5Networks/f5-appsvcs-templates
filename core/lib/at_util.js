f9bc32987fbf6c484990ee127098b6c1355cc9fb
Mitchell Stokes <M.Stokes@F5.com>
2020-01-13 16:11:28 -0800

    core: Switch at_util to class syntax

--- core/lib/at_util.js
+++ core/lib/at_util.js
@@ -11,12 +11,6 @@ const listApplications = d => fetchKeysOfType('Application', d);
 // eslint-disable-next-line no-unused-vars
 const getTenant = (d, k) => d[k];
 
-// as3 request object, intended to stream/lock later
-function ATRequest(device) {
-    this.device = device;
-    return this;
-}
-
 const makeRequest = (opts, body) => new Promise((resolve, reject) => {
     const payload = body ? JSON.stringify(body) : null;
     opts.headers = { 'Content-Type': 'application/json' };
@@ -70,92 +64,86 @@ const makeRequest = (opts, body) => new Promise((resolve, reject) => {
     req.end();
 });
 
-ATRequest.prototype.as3Info = function as3Info() {
-    const device = this.device;
+// as3 request object, intended to stream/lock later
+class ATRequest {
+    constructor(device) {
+        this.device = device;
+    }
 
-    const opts = {
-        host: device.ipaddress,
-        port: device.port,
-        auth: `${device.username}:${device.password}`,
-        rejectUnauthorized: false,
-        path: '/mgmt/shared/appsvcs/info'
-    };
-    return makeRequest(opts);
-};
+    as3Info() {
+        const device = this.device;
 
-ATRequest.prototype.tsInfo = function tsInfo() {
-    const device = this.device;
+        const opts = {
+            host: device.ipaddress,
+            port: device.port,
+            auth: `${device.username}:${device.password}`,
+            rejectUnauthorized: false,
+            path: '/mgmt/shared/appsvcs/info'
+        };
+        return makeRequest(opts);
+    }
 
-    const opts = {
-        host: device.ipaddress,
-        port: device.port,
-        auth: `${device.username}:${device.password}`,
-        rejectUnauthorized: false,
-        path: '/mgmt/shared/telemetry/info'
-    };
-    return makeRequest(opts);
-};
+    declaration(tenant) {
+        const device = this.device;
+        const t = tenant ? `/${tenant}` : '';
 
-ATRequest.prototype.declaration = function declaration(tenant) {
-    const device = this.device;
-    const t = tenant ? `/${tenant}` : '';
+        const opts = {
+            host: device.ipaddress,
+            port: device.port,
+            auth: `${device.username}:${device.password}`,
+            rejectUnauthorized: false,
+            path: `/mgmt/shared/appsvcs/declare${t}`
+        };
 
-    const opts = {
-        host: device.ipaddress,
-        port: device.port,
-        auth: `${device.username}:${device.password}`,
-        rejectUnauthorized: false,
-        path: `/mgmt/shared/appsvcs/declare${t}`
-    };
-
-    return makeRequest(opts).then((result) => {
-        if (result.status === 204) {
-            result.body = {
-                class: 'ADC',
-                schemaVersion: '3.11.0'
-            };
-        }
-        console.log('returning from as3 declaration GET', result);
-        return result;
-    });
-};
-
-ATRequest.prototype.declare = function declare(adc) {
-    const device = this.device;
-    const opts = {
-        host: device.ipaddress,
-        port: device.port,
-        auth: `${device.username}:${device.password}`,
-        rejectUnauthorized: false,
-        path: '/mgmt/shared/appsvcs/declare?showHash=true',
-        method: 'POST'
-    };
-
-    return makeRequest(opts, adc)
-        .then((result) => {
-            console.log(result);
-            if (result.status >= 400) {
-                if (result.body) {
-                    if (result.body.errors) {
-                        throw new Error(`${result.body.message}:${result.body.errors.join(', ')}`);
-                    }
-                } if (result.body.results) {
-                    throw new Error(JSON.stringify(result.body.results.map(r => `${r.message}:${r.response}`)));
-                } else {
-                    try {
-                        throw new Error(result.body.results.map(r => `${r.message}:${r.response}`));
-                    } catch (e) {
-                        throw new Error(`$$$$$${JSON.stringify(result.body)}$$$$$`);
-                    }
-                }
+        return makeRequest(opts).then((result) => {
+            if (result.status === 204) {
+                result.body = {
+                    class: 'ADC',
+                    schemaVersion: '3.11.0'
+                };
             }
-            if (!result.body.declaration && result.body.class !== 'ADC') {
-                throw new Error(`report this to as3 team please${JSON.stringify(result)}`);
-            }
-            console.log('returning from as3 declare POST', result);
+            console.log('returning from as3 declaration GET', result);
             return result;
         });
-};
+    }
+
+    declare(adc) {
+        const device = this.device;
+        const opts = {
+            host: device.ipaddress,
+            port: device.port,
+            auth: `${device.username}:${device.password}`,
+            rejectUnauthorized: false,
+            path: '/mgmt/shared/appsvcs/declare?showHash=true',
+            method: 'POST'
+        };
+
+        return makeRequest(opts, adc)
+            .then((result) => {
+                console.log(result);
+                if (result.status >= 400) {
+                    if (result.body) {
+                        if (result.body.errors) {
+                            throw new Error(`${result.body.message}:${result.body.errors.join(', ')}`);
+                        }
+                    } if (result.body.results) {
+                        throw new Error(JSON.stringify(result.body.results.map(r => `${r.message}:${r.response}`)));
+                    } else {
+                        try {
+                            throw new Error(result.body.results.map(r => `${r.message}:${r.response}`));
+                        } catch (e) {
+                            throw new Error(`$$$$$${JSON.stringify(result.body)}$$$$$`);
+                        }
+                    }
+                }
+                if (!result.body.declaration && result.body.class !== 'ADC') {
+                    throw new Error(`report this to as3 team please${JSON.stringify(result)}`);
+                }
+                console.log('returning from as3 declare POST', result);
+                return result;
+            });
+    }
+}
 
 module.exports = {
     listTenants,
