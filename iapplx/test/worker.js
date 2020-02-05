35a438c308cf1321fc4132c52338a7c13f1ee899
Mitchell Stokes <M.Stokes@F5.com>
2020-02-05 12:37:23 -0800

    iapplx/worker: Add test for DELETE on /applications endpoint
    This is currently unsupported and returns a 405.

--- iapplx/test/worker.js
+++ iapplx/test/worker.js
@@ -398,6 +398,14 @@ describe('template worker info tests', function () {
                 assert.notEqual(op.status, 404);
             });
     });
+    it('delete_all_apps', function () {
+        const worker = new TemplateWorker();
+        const op = new RestOp('applications');
+        return worker.onDelete(op)
+            .then(() => {
+                assert.strictEqual(op.status, 405);
+            });
+    });
     it('delete_bad_end_point', function () {
         const worker = new TemplateWorker();
         const op = new RestOp('bad');
