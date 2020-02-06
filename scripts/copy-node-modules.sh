ce361f7f66d2542ae156a328d16aa7c858d7096b
Mitchell Stokes <M.Stokes@F5.com>
2020-02-06 15:37:11 -0800

    scripts: Make copy-node-modules handle any number of "file:" packages
    It is also no longer specific to FAST.

--- scripts/copy-node-modules.sh
+++ scripts/copy-node-modules.sh
@@ -10,14 +10,17 @@ rm -rf "${tmpdir}"
 mkdir "${tmpdir}"
 cp "${src}"/package* "${tmpdir}"/
 pushd "${tmpdir}"
-module_path="${src}"/../core
-npm pack "${module_path}"
-module_pkg_name=$(node -e "console.log(require('${module_path}/package.json').name);")
-module_pkg_name=${module_pkg_name//@}
-module_pkg_name=${module_pkg_name//\//-}
-module_pkg_ver=$(node -e "console.log(require('${module_path}/package.json').version)")
-module_pkg=${module_pkg_name}-${module_pkg_ver}.tgz
-sed -i'.bu' "s%file:\.\./core%file:${module_pkg}%" package.json
+mapfile -t file_modules < <(grep 'file:' package.json | sed 's/\s*".*": "file:\(.*\)",/\1/')
+for file_module in "${file_modules[@]}"; do
+    module_path="${src}"/"${file_module}"
+    npm pack "${module_path}"
+    module_pkg_name=$(node -e "console.log(require('${module_path}/package.json').name);")
+    module_pkg_name=${module_pkg_name//@}
+    module_pkg_name=${module_pkg_name//\//-}
+    module_pkg_ver=$(node -e "console.log(require('${module_path}/package.json').version)")
+    module_pkg=${module_pkg_name}-${module_pkg_ver}.tgz
+    sed -i'.bu' "s%file:${file_module}%file:${module_pkg}%" package.json
+done
 npm install --prod --no-optional
 popd
 
