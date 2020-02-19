4951a1e98f36f1bcdb95c32a1e35eb164fcf5fb4
Mitchell Stokes <M.Stokes@F5.com>
2020-02-19 14:48:22 -0800

    core/FsSchemaProvider: Switch to class syntax

--- core/lib/schema_provider.js
+++ core/lib/schema_provider.js
@@ -4,33 +4,35 @@ const fs = require('fs');
 
 const ResourceCache = require('./resource_cache').ResourceCache;
 
-function FsSchemaProvider(schemaRootPath) {
-    this.schema_path = schemaRootPath;
-    this.cache = new ResourceCache(schemaName => new Promise((resolve, reject) => {
-        fs.readFile(`${schemaRootPath}/${schemaName}.json`, (err, data) => {
-            if (err) return reject(err);
-            return resolve(data.toString('utf8'));
-        });
-    }));
+class FsSchemaProvider {
+    constructor(schemaRootPath) {
+        this.schema_path = schemaRootPath;
+        this.cache = new ResourceCache(schemaName => new Promise((resolve, reject) => {
+            fs.readFile(`${schemaRootPath}/${schemaName}.json`, (err, data) => {
+                if (err) return reject(err);
+                return resolve(data.toString('utf8'));
+            });
+        }));
 
-    return this;
+        return this;
+    }
+
+    fetch(key) {
+        return this.cache.fetch(key);
+    }
+
+    list() {
+        return new Promise((resolve, reject) => {
+            fs.readdir(this.schema_path, (err, data) => {
+                if (err) return reject(err);
+
+                const list = data.filter(x => x.endsWith('.json'))
+                    .map(x => x.split('.')[0]);
+                return resolve(list);
+            });
+        });
+    }
 }
-
-FsSchemaProvider.prototype.fetch = function fetch(key) {
-    return this.cache.fetch(key);
-};
-
-FsSchemaProvider.prototype.list = function schemaList() {
-    return new Promise((resolve, reject) => {
-        fs.readdir(this.schema_path, (err, data) => {
-            if (err) return reject(err);
-
-            const list = data.filter(x => x.endsWith('.json'))
-                .map(x => x.split('.')[0]);
-            return resolve(list);
-        });
-    });
-};
 
 module.exports = {
     FsSchemaProvider
