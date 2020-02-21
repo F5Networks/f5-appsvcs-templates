b54b41356e3002190a6979b7dcc56f631d3b69a3
Mitchell Stokes <M.Stokes@F5.com>
2020-02-21 11:16:27 -0800

    core: Switch ResourceCache to class syntax

--- core/lib/resource_cache.js
+++ core/lib/resource_cache.js
@@ -1,28 +1,34 @@
 'use strict';
 
-function ResourceCache(asyncFetch) {
-    // used for caching AS3 TemplateEngine objects
-    this.cached = {};
-    this.cache_limit = 100;
-    this.asyncFetch = asyncFetch;
-    return this;
-}
+class ResourceCache {
+    constructor(asyncFetch) {
+        // used for caching AS3 TemplateEngine objects
+        this.cached = {};
+        this.cache_limit = 100;
+        this.asyncFetch = asyncFetch;
+        return this;
+    }
 
-ResourceCache.prototype.fetch = function fetch(key) {
-    return (() => {
-        if (!this.cached[key]) {
-            return this.asyncFetch(key)
-                .then((resource) => {
-                    this.cached[key] = resource;
-                    const allKeys = Object.keys(this.cached);
-                    const oldestKey = allKeys.shift();
-                    if (allKeys.length > this.cache_limit) delete this.cached[oldestKey];
-                    return resource;
-                });
-        }
-        return Promise.resolve(this.cached[key]);
-    })();
-};
+    fetch(key) {
+        return (() => {
+            if (!this.cached[key]) {
+                return this.asyncFetch(key)
+                    .then((resource) => {
+                        this.cached[key] = resource;
+                        const allKeys = Object.keys(this.cached);
+                        const oldestKey = allKeys.shift();
+                        if (allKeys.length > this.cache_limit) delete this.cached[oldestKey];
+                        return resource;
+                    });
+            }
+            return Promise.resolve(this.cached[key]);
+        })();
+    }
+
+    invalidate() {
+        this.cached = {};
+    }
+}
 
 module.exports = {
     ResourceCache
