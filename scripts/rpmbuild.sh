9c20d76ebc7d34a3409b832709075e06c564a59d
Mitchell Stokes <M.Stokes@F5.com>
2020-02-28 15:36:45 -0800

    rpmbuild: Another version fix

--- scripts/rpmbuild.sh
+++ scripts/rpmbuild.sh
@@ -8,7 +8,7 @@ VERSION="$(echo $FULL_VERSION | cut -d - -f 1)"
 LAST_TAG="$(git describe --tags --always --abbrev=0)"
 NUM_COMMITS_FROM_TAG=$(git rev-list $LAST_TAG.. --count)
 if [[ "$LAST_TAG" == "v${VERSION}"* ]]; then
-    VERSION="$LAST_TAG"
+    VERSION="$(echo $LAST_TAG | tail -c +2)"
 fi
 if [ "$NUM_COMMITS_FROM_TAG" -ne 0 ]; then
     VERSION="$VERSION.dev$NUM_COMMITS_FROM_TAG"
