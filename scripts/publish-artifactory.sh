21127afa64f293977480e6968b3fe6f565e80f67
Mitchell Stokes <M.Stokes@F5.com>
2020-03-16 13:15:48 -0700

    scripts/publish-artifactory: Upload the whole dist directory

--- scripts/publish-artifactory.sh
+++ scripts/publish-artifactory.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -eux
 
+upload_file () {
+    echo uploading "$1"
+    # upload file
+    curl $curl_flags -T "dist/$1" "$repo_url/$1"
+    # artifactory calculates and compares the checksums but strips the filename
+    curl $curl_flags -T "dist/$1.sha256" "$repo_url/$1.sha256"
+    # sha file with hash and file name preserved
+    curl $curl_flags -T "dist/$1.sha256" "$repo_url/$1.sha256.txt"
+}
+
 package_file="${1:-package.json}"
 product_name=$(jq -r .name "$package_file")
 product_version=$(jq -r .version "$package_file")
@@ -8,14 +18,13 @@ product_version=$(jq -r .version "$package_file")
 repo_url="https://$ARTIFACTORY_URL/artifactory/f5-automation-toolchain-generic/$product_name/$product_version"
 curl_flags="--insecure --fail -H X-JFrog-Art-Api:$ARTIFACTORY_API_KEY"
 
-# upload RPM
-cd $(dirname $package_file)
+# find files
 pushd dist
-rpm_name=$(ls -t $product_name-${product_version}*.rpm 2>/dev/null | head -1)
+files=$(ls --hide=*.sha256)
 popd
-curl $curl_flags -T "dist/$rpm_name" "$repo_url/$rpm_name"
-# artifactory calculates and compares the checksums but strips the filename
-curl $curl_flags -T "dist/$rpm_name.sha256" "$repo_url/$rpm_name.sha256"
-# sha file with hash and file name preserved
-curl $curl_flags -T "dist/$rpm_name.sha256" "$repo_url/$rpm_name.sha256.txt"
 
+# upload each file
+for f in $files
+do
+    upload_file $f
+done
