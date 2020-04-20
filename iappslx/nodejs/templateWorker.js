b3729a12a2860357e4d55fc535085b544a1013c8
Mitchell Stokes <M.Stokes@F5.com>
2020-04-20 11:52:57 -0700

    templates/example: Strip out unused user-defined types and rename f5.json to types.json
    This helps differentiate it from the f5.json in bigip-fast-templates.

--- iappslx/nodejs/templateWorker.js
+++ iappslx/nodejs/templateWorker.js
@@ -38,7 +38,7 @@ const supportedHashes = {
         '98fa9609dac9b3bd9afd245644a24821372799691dfc87992af3e0fed90fdf39' //  v1.0
     ],
     examples: [
-        'd0e708656e03573db98b96924e34ec22c1adfc71a6a78e3fa780d51c2b5dede5' //  v1.0
+        'fffa80a94a8ccd2edd3cdea406c68183c1aada851a200268bca5e2aad9f33e66' //  v1.0
     ]
 };
 
