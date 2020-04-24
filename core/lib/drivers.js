31a778ced32e6f260ea738449a580483399703c5
Mitchell Stokes <M.Stokes@F5.com>
2020-04-24 09:34:42 -0700

    core/drivers: Fix lint error

--- core/lib/drivers.js
+++ core/lib/drivers.js
@@ -58,7 +58,7 @@ class AS3Driver {
 
     _appFromDecl(declaration, tenant, app) {
         const as3App = declaration[tenant][app];
-        const fastApp = as3App.constants && as3App.constants[AS3DriverConstantsKey] || {};
+        const fastApp = (as3App.constants && as3App.constants[AS3DriverConstantsKey]) || {};
         return Object.assign({}, fastApp, {
             tenant,
             name: app
