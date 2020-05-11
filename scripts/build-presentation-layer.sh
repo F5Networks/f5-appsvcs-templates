8fe3baae42d3633e6152d78ddd9b7b949a75f01b
Mitchell Stokes <M.Stokes@F5.com>
2020-05-11 09:52:25 -0700

    gui: Stop converting the openapi spec to JSON
    This step is no longer necessary.

--- scripts/build-presentation-layer.sh
+++ scripts/build-presentation-layer.sh
@@ -2,8 +2,7 @@
 set -eux
 
 # Generate API docs
-npx js-yaml ../docs/openapi.yml > presentation/openapi.json
-npx redoc-cli bundle -o presentation/apidoc.html presentation/openapi.json
+npx redoc-cli bundle -o presentation/apidoc.html ../docs/openapi.yml
 
 # Bundle Node modules
 npx browserify presentation/app.js -o presentation/bundle.js
