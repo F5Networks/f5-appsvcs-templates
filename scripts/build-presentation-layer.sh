ab1b2f5ee289a2214f989c0dee32401381a6268e
Mitchell Stokes <M.Stokes@F5.com>
2020-07-29 09:50:30 -0700

    docs/openapi: Fix server URL to not use a relative path on clouddocs
    The API docs in the FAST GUI still use a relative path.

--- scripts/build-presentation-layer.sh
+++ scripts/build-presentation-layer.sh
@@ -2,7 +2,10 @@
 set -eux
 
 # Generate API docs
-npx redoc-cli bundle -o iappslx/presentation/apidoc.html docs/openapi.yml
+cp docs/openapi.yml openapi-tmp.yml
+sed -i'.bu' "s%http://localhost:8100/mgmt/shared/fast%/mgmt/shared/fast%" openapi-tmp.yml
+npx redoc-cli bundle -o iappslx/presentation/apidoc.html openapi-tmp.yml
+rm openapi-tmp.*
 
 # Bundle Node modules
 npx browserify iappslx/presentation/app.js -o iappslx/presentation/bundle.js
