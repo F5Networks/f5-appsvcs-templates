49e2e2c0a8496ae9d0dd5161d33b47b02e28a1aa
Mitchell Stokes <M.Stokes@F5.com>
2020-09-09 11:37:30 -0700

    gui/templates: Switch from elements module to Vue

--- iappslx/presentation/app.js
+++ iappslx/presentation/app.js
@@ -11,15 +11,8 @@ const { Template, guiUtils } = require('@f5devcentral/f5-fast-core');
 
 const UiWorker = require('./lib/ui-worker.js');
 const {
-    Div,
-    Span,
     Clickable,
-    Icon,
-    Row,
-    Modal,
-    Td,
-    Expandable,
-    Svg
+    Expandable
 } = require('./lib/elements');
 
 const endPointUrl = '/mgmt/shared/fast';
@@ -430,244 +423,155 @@ route('templates', 'templates', () => {
             .catch(e => dispOutput(`Failed to install ${tsName}:\n${e.message}`));
     };
     document.getElementById('btn-delete-all-ts').onclick = () => {
-        new Modal().setTitle('Warning').setMessage('All Template Sets will be removed!').setOkFunction(() => {
-            dispOutput('Deleting All Template Sets');
-            return safeFetch(`${endPointUrl}/templatesets`, {
-                method: 'DELETE'
-            })
-                .then(() => {
-                    dispOutput('All Template Sets deleted successfully');
-                    window.location.reload();
+        vueApp.showModal(
+            'warning',
+            'All Template Sets will be removed!',
+            () => {
+                dispOutput('Deleting All Template Sets');
+                return safeFetch(`${endPointUrl}/templatesets`, {
+                    method: 'DELETE'
                 })
-                .catch(e => dispOutput(`Failed to delete all Template Sets. Error: ${e.message}`));
-        })
-            .appendToParent(document.getElementById('app'));
+                    .then(() => {
+                        dispOutput('All Template Sets deleted successfully');
+                        window.location.reload();
+                    })
+                    .catch(e => dispOutput(`Failed to delete all Template Sets. Error: ${e.message}`));
+            }
+        );
+    };
+    const filters = {
+        enabled: 'Enabled',
+        f5: 'F5 Supported',
+        disabled: 'Disabled',
+        all: 'All Template Sets'
+    };
+    const templatesFilterKey = 'templates-filter';
+    let currentFilter = UiWorker.getStore(templatesFilterKey);
+    if (!currentFilter) {
+        currentFilter = 'enabled';
+    }
+
+    vueApp.$refs.page.setFilter = (filter) => {
+        appState.data.currentFilter = filter;
+        UiWorker.store(templatesFilterKey, filter);
+        window.location.reload();
     };
 
-    const templatesFilterKey = 'templates-filter';
-    const templatesFilterElem = document.getElementById('templates-filter');
-    const filterElem = document.getElementById('filter');
-    const menu = templatesFilterElem.getElementsByClassName('menu')[0];
-    const selected = templatesFilterElem.getElementsByClassName('selected')[0];
-    if (!UiWorker.getStore(templatesFilterKey)) {
-        UiWorker.store(templatesFilterKey, selected.id);
-        console.log('templatesFilter empty in store. Pulled from  stored:', selected.id);
-    }
+    vueApp.$refs.page.removeSet = (setName) => {
+        vueApp.showModal(
+            'warning',
+            `Template Set '${setName}' will be removed!`,
+            () => {
+                dispOutput(`Deleting ${setName}`);
+                return Promise.resolve()
+                    .then(() => safeFetch(`${endPointUrl}/templatesets/${setName}`, {
+                        method: 'DELETE'
+                    }))
+                    .then(() => {
+                        dispOutput(`${setName} deleted successfully`);
+                        window.location.reload();
+                    })
+                    .catch(err => dispOutput(`Failed to delete ${setName}:\n${err.message}`));
+            }
+        );
+    };
 
-    const curFilter = UiWorker.getStore(templatesFilterKey);
-    if (document.getElementById(curFilter).innerText.toLowerCase() !== templatesFilterElem.innerText.toLowerCase()) {
-        selected.classList.remove('selected');
-        document.getElementById(curFilter).classList.add('selected');
-    }
+    vueApp.$refs.page.installSet = (setName) => {
+        vueApp.showModal(
+            'info',
+            `Template Set '${setName}' will be enabled.`,
+            () => {
+                dispOutput(`Enabling ${setName}`);
+                return Promise.resolve()
+                    .then(() => safeFetch(`${endPointUrl}/templatesets`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            name: setName
+                        })
+                    }))
+                    .then(() => {
+                        dispOutput(`${setName} enabled successfully`);
+                        window.location.reload();
+                    })
+                    .catch(err => dispOutput(`Failed to enable ${setName}:\n${err.message}`));
+            }
+        );
+    };
 
-    UiWorker.iterateHtmlCollection(menu, (item) => {
-        item.onclick = () => {
-            UiWorker.store(templatesFilterKey, item.id);
-            window.location.reload();
-        };
-    });
+    vueApp.$refs.page.updateSet = (setName) => {
+        vueApp.showModal(
+            'warning',
+            `Template Set '${setName}' will be updated!`,
+            () => {
+                dispOutput(`Updating ${setName}`);
+                return Promise.resolve()
+                    .then(() => safeFetch(`${endPointUrl}/templatesets`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            name: setName
+                        })
+                    }))
+                    .then(() => {
+                        dispOutput(`${setName} installed successfully`);
+                        window.location.reload();
+                    })
+                    .catch(err => dispOutput(`Failed to install ${setName}:\n${err.message}`));
+            }
+        );
+    };
 
-    filterElem.innerText = document.getElementById(curFilter).innerText;
+    appState.data = {
+        filters,
+        currentFilter,
+        sets: [],
+        apps: {}
+    };
 
-    const templateDiv = document.getElementById('template-list');
     return Promise.all([
         getJSON('applications'),
         getJSON('templatesets'),
         getJSON('templatesets?showDisabled=true')
     ])
         .then(([applications, templatesets, disabledTemplateSets]) => {
-            const filter = document.getElementById('templates-filter').getElementsByClassName('selected')[0].id.toLowerCase();
-            const allTemplates = templatesets.concat(disabledTemplateSets);
-            const setMap = allTemplates.reduce((acc, curr) => {
-                if (filter === 'all') acc[curr.name] = curr;
-                if (filter === 'f5' && curr.supported) acc[curr.name] = curr;
-                if (filter === 'enabled' && curr.enabled) acc[curr.name] = curr;
-                if (filter === 'disabled' && !curr.enabled) acc[curr.name] = curr;
+            const allTemplates = templatesets
+                .concat(disabledTemplateSets)
+                .filter(x => (
+                    (currentFilter === 'all')
+                    || (currentFilter === 'f5' && x.supported)
+                    || (currentFilter === 'enabled' && x.enabled)
+                    || (currentFilter === 'disabled' && !x.enabled)
+                ));
+
+            // build dictionary of app lists, keyed by set
+            const appDict = applications.reduce((acc, curr) => {
+                const setName = curr.template.split('/')[0];
+                if (!acc[setName]) {
+                    acc[setName] = [];
+                }
+                acc[setName].push(curr);
                 return acc;
             }, {});
 
-            // build dictionary of app lists, keyed by template
-            const appDict = applications.reduce((a, c) => {
-                if (c.template) {
-                    if (!a[c.template]) {
-                        a[c.template] = [];
-                    }
-                    a[c.template].push(c);
-                }
-                return a;
+            const setMap = allTemplates.reduce((acc, curr) => {
+                const apps = appDict[curr.name] || [];
+                acc[curr.name] = Object.assign(curr, {
+                    expanded: apps.length < 3,
+                    apps
+                });
+                return acc;
             }, {});
 
+            appState.data.sets = setMap;
             Object.values(setMap).forEach((setData) => {
-                const setName = setData.name;
-                const setActions = {
-                    Remove: (e) => {
-                        new Modal().setTitle('Warning').setMessage(`Template Set '${setName}' will be removed!`).setOkFunction(() => {
-                            dispOutput(`Deleting ${setName}`);
-                            return safeFetch(`${endPointUrl}/templatesets/${setName}`, {
-                                method: 'DELETE'
-                            })
-                                .then(() => {
-                                    dispOutput(`${setName} deleted successfully`);
-                                    window.location.reload();
-                                })
-                                .catch(err => dispOutput(`Failed to delete ${setName}:\n${err.message}`));
-                        })
-                            .appendToParent(document.getElementById('app'));
-
-                        e.stopPropagation();
-                    }
-                };
-
-                if (!setData.enabled) {
-                    setActions.Install = (e) => {
-                        new Modal().setTitle('Enabling Template Set').setMessage(`Template Set '${setName}' will be enabled.`).setOkFunction(() => {
-                            dispOutput(`Enabling ${setName}`);
-                            return Promise.resolve()
-                                .then(() => safeFetch(`${endPointUrl}/templatesets`, {
-                                    method: 'POST',
-                                    headers: {
-                                        'Content-Type': 'application/json'
-                                    },
-                                    body: JSON.stringify({
-                                        name: setName
-                                    })
-                                }))
-                                .then(() => {
-                                    dispOutput(`${setName} enabled successfully`);
-                                    window.location.reload();
-                                })
-                                .catch(err => dispOutput(`Failed to enable ${setName}:\n${err.message}`));
-                        })
-                            .appendToParent(document.getElementById('app'));
-
-                        e.stopPropagation();
-                    };
+                if (!setData.enabled && setData.updateAvailable) {
+                    console.error('enabled === false && updateAvailable is illegal. Critical Error');
                 }
-
-                if (setData.updateAvailable) {
-                    setActions.Update = (e) => {
-                        new Modal().setTitle('Warning').setMessage(`Template Set '${setName}' will be updated!`).setOkFunction(() => {
-                            dispOutput(`Updating ${setName}`);
-                            return Promise.resolve()
-                                .then(() => safeFetch(`${endPointUrl}/templatesets`, {
-                                    method: 'POST',
-                                    headers: {
-                                        'Content-Type': 'application/json'
-                                    },
-                                    body: JSON.stringify({
-                                        name: setName
-                                    })
-                                }))
-                                .then(() => {
-                                    dispOutput(`${setName} installed successfully`);
-                                    window.location.reload();
-                                })
-                                .catch(err => dispOutput(`Failed to install ${setName}:\n${err.message}`));
-                        })
-                            .appendToParent(document.getElementById('app'));
-
-                        e.stopPropagation();
-                    };
-                }
-
-                if (!setData.enabled && setData.updateAvailable) { console.error('enabled === false && updateAvailable is illegal. Critical Error'); }
-
-                const templateSetRow = new Row('tr row-dark clickable').setId(setName).appendToParent(templateDiv).setColumns([
-                    () => {
-                        const tempSetName = new Div('td td-template-set').setChildren([
-                            new Icon('fa-angle-right').html(),
-                            `${setName}&nbsp`
-                        ]);
-                        const traitsDiv = new Span('traits');
-                        if (setData.supported) {
-                            new Svg('f5-icon').setToolstrip('Template Set Supported by F5')
-                                .setClassList('tooltipped-f5-icon').appendToParent(traitsDiv);
-                        }
-                        if (setData.enabled) {
-                            new Icon('fa-check-circle').setToolstrip('Template Set is Enabled').appendToParent(traitsDiv);
-                        } else { // can a template set be removable if it's disabled?
-                            new Icon('fa-times-circle').setClassList('red-base-forecolor').setToolstrip('Template Set is Disabled!')
-                                .setClassList('tooltip-red')
-                                .appendToParent(traitsDiv);
-                        }
-                        traitsDiv.appendToParent(tempSetName);
-                        return tempSetName;
-                    },
-                    new Td(),
-                    () => {
-                        const actions = new Td();
-
-                        if (!setData.enabled) {
-                            const installBtn = new Clickable('icon:fa-download').setOnClick(setActions.Install).setToolstrip('Install Template Set');
-                            actions.safeAppend(installBtn);
-                            return actions;
-                        }
-
-                        Object.entries(setActions).forEach(([actName, actFn]) => {
-                            const iconClass = (actName.toLowerCase() === 'update') ? 'fa-edit' : 'fa-trash';
-                            actions.safeAppend(new Clickable(`icon:${iconClass}`).setOnClick(actFn).setToolstrip(`${actName} Template Set`));
-                        });
-                        return actions;
-                    }
-                ]);
-
-                const tempSetChild = `${setName}-child`;
-                templateSetRow.makeExpandable(tempSetChild);
-
-                if (!setData.enabled) { templateSetRow.setClassList('row-dark-red red-hover'); }
-
-                setMap[setName].templates.forEach((tmpl) => {
-                    const templateName = tmpl.name;
-                    const appList = appDict[tmpl.name] || [];
-
-                    const templateRow = new Row('tr row-light').setClassList(tempSetChild).appendToParent(templateDiv);
-                    templateRow.setColumns([
-                        templateName,
-                        () => {
-                            const applicationsDiv = () => {
-                                const applicationsMapped = appList.map(app => `${app.tenant} ${app.name}`);
-                                const div = new Div();
-                                applicationsMapped.forEach((item) => {
-                                    const tenantApp = item.split(' ');
-                                    new Div('fontsize-6rem').setChildren([
-                                        tenantApp[0],
-                                        new Icon('fa-angle-double-right').setClassList('fontsize-5rem').html(),
-                                        tenantApp[1]
-                                    ]).appendToParent(div);
-                                });
-                                return div;
-                            };
-
-                            const appsTd = new Td().appendToParent(templateRow);
-                            if (appList.length < 3) {
-                                appsTd.safeAppend(applicationsDiv());
-                            } else {
-                                appsTd.setClassList('italic').setInnerText('*click to view*');
-                                templateRow.setClassList('clickable');
-                                templateRow.elem.onclick = () => {
-                                    if (appsTd.elem.innerText === '*click to view*') {
-                                        UiWorker.destroyChildren(appsTd);
-                                        appsTd.elem.innerText = '';
-                                        appsTd.elem.classList.remove('italic');
-                                        appsTd.safeAppend(applicationsDiv());
-                                    } else {
-                                        UiWorker.destroyChildren(appsTd.elem);
-                                        appsTd.elem.innerText = '*click to view*';
-                                        appsTd.elem.classList.add('italic');
-                                    }
-                                };
-                            }
-                            return appsTd;
-                        },
-                        new Td()
-                    ]);
-
-                    if (!templateSetRow.elem.classList.contains('expanded')) { templateRow.elem.classList.add('display-none'); }
-
-                    if (!setData.enabled) {
-                        templateRow.setClassList('row-light-red grey-forecolor');
-                    }
-                });
             });
         })
         .then(() => dispOutput(''));
