9fb1f6c70a6e29a7a3717c58d49baaf5dc596a1b
Mitchell Stokes <M.Stokes@F5.com>
2020-09-10 08:10:43 -0700

    scripts/publish-artifactory.sh: Copy changes from atg-build
    This fixes the extra "dist/" in generated hashes.

--- scripts/publish-artifactory.sh
+++ scripts/publish-artifactory.sh
@@ -6,12 +6,11 @@ product_name=$(jq -r .name "$package_file")
 product_version=$(jq -r .version "$package_file" | cut -d - -f 1)
 
 repo_url="https://$ARTIFACTORY_URL/artifactory/f5-automation-toolchain-generic/$product_name/$product_version"
-curl_flags="--insecure --fail -H X-JFrog-Art-Api:$ARTIFACTORY_API_KEY"
+curl_flags="--fail -H X-JFrog-Art-Api:$ARTIFACTORY_API_KEY"
 
 # find files
-pushd dist
+cd dist
 files=$(ls --hide=*.sha256)
-popd
 
 # upload each file
 for f in $files
@@ -19,16 +18,16 @@ do
     echo uploading "$f"
 
     # upload file
-    curl $curl_flags -T "dist/$f" "$repo_url/$f"
+    curl $curl_flags -T "$f" "$repo_url/$f"
 
     # calculate and upload checksums
     for chksm in sha256 sha1 md5; do
-        "${chksm}sum" "dist/$f" > "dist/$f.$chksm"
-        curl $curl_flags -T "dist/$f.$chksm" "$repo_url/$f.$chksm"
+        "${chksm}sum" "$f" > "$f.$chksm"
+        curl $curl_flags -T "$f.$chksm" "$repo_url/$f.$chksm"
         if [ "$chksm" = "sha256" ]; then
             # sha file with hash and file name preserved
-            curl $curl_flags -T "dist/$f.sha256" "$repo_url/$f.sha256.txt"
-        rm "dist/$f.$chksm"
+            curl $curl_flags -T "$f.sha256" "$repo_url/$f.sha256.txt"
+        rm "$f.$chksm"
         fi
     done
 done
