58c003e9b82e001c70014f7086b2d16c15f01630
slater <f.slater@f5.com>
2020-09-17 12:35:19 -0700

    fix dns template

--- iappslx/nodejs/fastWorker.js
+++ iappslx/nodejs/fastWorker.js
@@ -45,7 +45,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
-        '9939d18b02151c6cd0da82a4db7810bffc80b24a8f9704ab714a6988a38a4dcc', // v1.4
+        '2cc771fb6a17d8d4264df606a1627983955381668ac8b88d5306826488167650', // v1.4
         'e7eba47ac564fdc6d5ae8ae4c5eb6de3d9d22673a55a2e928ab59c8c8e16376b', // v1.3
         '316653656cfd60a256d9b92820b2f702819523496db8ca01ae3adec3bd05f08c', // v1.2
         '985f9cd58299a35e83851e46ba7f4f2b1b0175cad697bed09397e0e07ad59217' //  v1.0
