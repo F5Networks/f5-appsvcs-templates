333a626228835ec373e25bbde9147177399f859e
Mitchell Stokes <M.Stokes@F5.com>
2020-10-12 15:22:24 -0700

    bigip-fast-templates: Add Exchange and Sharepoint templates
    These are from https://github.com/f5devcentral/f5-bd-FASTexample

--- iappslx/nodejs/fastWorker.js
+++ iappslx/nodejs/fastWorker.js
@@ -44,6 +44,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
+        '766b3827ea58b41c74d04a6d5065f3c8713edaaa2cba36ec385433b2304f22a4', // v1.5
         '99bf347ba5556df2e8c7100a97ea4c24171e436ed9f5dc9dfb446387f29e0bfe', // v1.4
         'e7eba47ac564fdc6d5ae8ae4c5eb6de3d9d22673a55a2e928ab59c8c8e16376b', // v1.3
         '316653656cfd60a256d9b92820b2f702819523496db8ca01ae3adec3bd05f08c', // v1.2
