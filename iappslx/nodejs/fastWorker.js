26bf968fa7c6797de1796aa69b0dc77316bdd903
Mitchell Stokes <M.Stokes@F5.com>
2021-02-12 12:07:29 -0800

    bigip-fast-templates: Fix referencing TS log profiles in /Common/Shared

--- iappslx/nodejs/fastWorker.js
+++ iappslx/nodejs/fastWorker.js
@@ -79,7 +79,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
-        'bcecc27095fce67a7f1a72421cf34682c711dec4886d63ac723183ceeaf13e4f', // v1.7
+        '7291841b1ef815f7f3bd6fc60922ddaad60c728daa36a607c6325399e7499e30', // v1.7
         '9b65c17982fd5f83a36576c1a55f2771a0011283db8221704925ee803b8dbd13', // v1.6
         '48316eb5f20c6f3bc4e78ad50b0d82fae46fc3c7fa615fe438ff8f84b3a3c2ea', // v1.5
         '99bf347ba5556df2e8c7100a97ea4c24171e436ed9f5dc9dfb446387f29e0bfe', // v1.4
