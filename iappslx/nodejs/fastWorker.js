599385fa227a7c519662a8661b8551f52d95d805
Mitchell Stokes <M.Stokes@F5.com>
2021-02-17 09:44:29 -0800

    bigip-fast-templates/microsoft_adfs: Fix using TLS Server without TLS Client

--- iappslx/nodejs/fastWorker.js
+++ iappslx/nodejs/fastWorker.js
@@ -79,7 +79,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
-        '790b85a848514524c84ea593e1a0989942a7b17f6ecfa0557d52dc0c06927e76', // v1.7
+        'ba93c91707601a26dab364d5b1af5fc0821f1e3b90c90b29407e6070db1e49f6', // v1.7
         '9b65c17982fd5f83a36576c1a55f2771a0011283db8221704925ee803b8dbd13', // v1.6
         '48316eb5f20c6f3bc4e78ad50b0d82fae46fc3c7fa615fe438ff8f84b3a3c2ea', // v1.5
         '99bf347ba5556df2e8c7100a97ea4c24171e436ed9f5dc9dfb446387f29e0bfe', // v1.4
