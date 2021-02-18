94df508dab66ec11297d50b1c3263db8cf16c9f0
slater <f.slater@f5.com>
2021-02-18 09:28:57 -0800

    fix: irule test

--- iappslx/nodejs/fastWorker.js
+++ iappslx/nodejs/fastWorker.js
@@ -79,7 +79,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
-        'fb5ef8e4b07abb4e556ff105795238d0d0930a644fe72a5a8fd063dcc2d4a9d3', // v1.7
+        'eb5f7a13d0e5de697fe02d8112e1947608e676e5c44d05a9911b75b3321782a0', // v1.7
         '9b65c17982fd5f83a36576c1a55f2771a0011283db8221704925ee803b8dbd13', // v1.6
         '48316eb5f20c6f3bc4e78ad50b0d82fae46fc3c7fa615fe438ff8f84b3a3c2ea', // v1.5
         '99bf347ba5556df2e8c7100a97ea4c24171e436ed9f5dc9dfb446387f29e0bfe', // v1.4
