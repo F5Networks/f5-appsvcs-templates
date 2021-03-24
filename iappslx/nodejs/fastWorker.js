96066ed0ea778bbf3a05de28e1cbded8a5185ae3
Mitchell Stokes <M.Stokes@F5.com>
2021-03-24 14:03:12 -0700

    bigip-fast-templates/_vlan_allow: Fix indentation issues

--- iappslx/nodejs/fastWorker.js
+++ iappslx/nodejs/fastWorker.js
@@ -77,7 +77,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
-        '59fc4a24b4dea268640744fa7187a928e7a2efcff870efa492be19c7a192bc75', // v1.8
+        'e7c530a34d35170fa34eba8a7543aa4a90917a72fb7b7be991ce08e3fcf9b6ae', // v1.8
         '83df55f23066fd0ac205ce3dca2c96ffd71e459914d7dcf205f3201fb1570427', // v1.7
         '9b65c17982fd5f83a36576c1a55f2771a0011283db8221704925ee803b8dbd13', // v1.6
         '48316eb5f20c6f3bc4e78ad50b0d82fae46fc3c7fa615fe438ff8f84b3a3c2ea', // v1.5
