51ef75b030f94c12fae70d0cc271aaf69efa344c
Mitchell Stokes <M.Stokes@F5.com>
2021-04-26 08:50:33 -0700

    webpack: Setup a CSS loader for Vue SFCs

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,13 @@ module.exports = {
             {
                 test: /\.vue$/,
                 loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
             }
         ]
     },
