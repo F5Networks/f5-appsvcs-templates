504216e91130456de8f8c7e3bbf49b9ce6330867
Mitchell Stokes <M.Stokes@F5.com>
2021-05-03 11:39:12 -0700

    gui: Add SortedTable component
    This is a generic component that can be used to show data that can be
    sorted based on column headers.

---
+++ presentation/components/SortedTable.vue
@@ -0,0 +1,172 @@
+<template>
+    <div class="sorted-table">
+        <table>
+            <thead>
+                <tr>
+                    <th
+                        v-if="checkboxes"
+                        class="checkbox-col"
+                    >
+                        <input
+                            v-model="selAll"
+                            type="checkbox"
+                        >
+                    </th>
+                    <th
+                        v-for="column in columnNames"
+                        :key="column"
+                        @click="sort(column)"
+                    >
+                        <span class="fas fa-sort" />
+                        {{ column }}
+                    </th>
+                </tr>
+            </thead>
+            <tr
+                v-for="row in sortedRows"
+                :key="getRowKey(row)"
+            >
+                <td
+                    v-if="checkboxes"
+                    class="checkbox-col"
+                >
+                    <input
+                        v-model="selectedRows"
+                        :value="row"
+                        type="checkbox"
+                    >
+                </td>
+                <td
+                    v-for="prop in columns"
+                    :key="prop.property || prop"
+                >
+                    <span v-if="typeof(prop) === 'object'">
+                        <router-link :to="createLink(prop, row)">{{ row[prop.property] }}</router-link>
+                    </span>
+                    <span v-else>{{ row[prop] }}</span>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+// eslint-disable-next-line import/no-extraneous-dependencies
+const Mustache = require('mustache');
+
+export default {
+    name: 'SortedTable',
+    props: {
+        tableData: {
+            type: Array,
+            required: true
+        },
+        columns: {
+            type: Object,
+            required: true
+        },
+        checkboxes: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            currentKey: '',
+            currentDir: 'asc',
+            selectedRows: [],
+            selAll: false
+        };
+    },
+    computed: {
+        columnNames() {
+            return Object.keys(this.columns);
+        },
+        sortedRows() {
+            return this.tableData.slice().sort((a, b) => {
+                const dir = (this.currentDir === 'asc') ? 1 : -1;
+                const column = this.columns[this.currentKey];
+                const prop = column.property || column;
+                if (a[prop] < b[prop]) return -1 * dir;
+                if (a[prop] > b[prop]) return 1 * dir;
+                return 0;
+            });
+        }
+    },
+    watch: {
+        selAll(val) {
+            if (val) {
+                this.selectedRows = this.sortedRows;
+            } else {
+                this.selectedRows = [];
+            }
+        }
+    },
+    created() {
+        this.currentKey = this.columnNames[0];
+    },
+    methods: {
+        sort(key) {
+            if (key === this.currentKey) {
+                // Clicked again, switch direction
+                this.currentDir = (this.currentDir === 'asc') ? 'dsc' : 'asc';
+            }
+
+            this.currentKey = key;
+        },
+        createLink(prop, row) {
+            return Mustache.render(prop.link, { row });
+        },
+        getRowKey(row) {
+            const keys = Object.values(this.columns)
+                .map(x => x.property || x);
+            return keys.reduce((acc, cur) => acc + row[cur], '');
+        }
+    }
+};
+</script>
+
+<style scoped>
+table {
+    width: 100%;
+    border: 2px solid #b6b6b4;
+    border-collapse: collapse;
+    margin: 0.5em;
+}
+
+.checkbox-col {
+    cursor: initial;
+    width: 2em;
+    padding: 0.3em;
+}
+
+th {
+    text-align: left;
+    background-image: linear-gradient(#d3d2cf, #bab9b4);
+    border-left: 2px solid #b6b6b4;
+    padding: 0.3em;
+    cursor: pointer;
+}
+
+tr {
+    background-color: #ffffff;
+    border-bottom: 2px solid #b6b6b4;
+}
+
+td {
+    padding: 0.3em 0.3em 0.3em 1.3em;
+}
+
+a {
+    color: #428bca;
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+
+a:visited {
+    color: #428bca;
+}
+</style>
