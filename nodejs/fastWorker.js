df450f23317fbfc760fb59d56b5d8522e0327fa3
Mitchell Stokes <M.Stokes@F5.com>
2021-06-01 09:49:09 -0700

    bigip-fast-templates: Add titles to fragments for easier debugging

--- nodejs/fastWorker.js
+++ nodejs/fastWorker.js
@@ -84,6 +84,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
+        'cbdba39ee80b5eeaa5a0b4b6c45606f9c47617285df46b8984c03a907bb7c6cd', // 1.10
         '89f6d8fb68435c93748de3f175f208714dcbd75de37d9286a923656971c939f0', // v1.9
         'fbaee3fd9ecce14a2d90df8c155998749b49126e0eb80267e9b426c58677a164', // v1.8.1
         '42650496f8e1b00a7e8e6a7c148a781bb4204e95f09f66d7d89af5793ae0b8b7', // v1.8
