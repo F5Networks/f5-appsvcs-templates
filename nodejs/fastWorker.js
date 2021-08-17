cdaa119c7ea1d3b3aa0a07a4e0c4907d765f185e
Joel Keener <j.keener@f5.com>
2021-08-17 16:59:14 -0700

    reuse virtual template in dns

--- nodejs/fastWorker.js
+++ nodejs/fastWorker.js
@@ -86,7 +86,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
-        'b39db52b23bf7d901fe3cfa425c20b7051e79812c370ccb82c129678e043f815', // 1.11
+        '3da5492fd547a209f655314b89ab66d06967d06a508a0ccd40f674b7319e9329', // 1.11
         '64d9692bdab5f1e2ba835700df4d719662b9976b9ff094fe7879f74d411fe00b', // 1.10
         '89f6d8fb68435c93748de3f175f208714dcbd75de37d9286a923656971c939f0', // v1.9
         'fbaee3fd9ecce14a2d90df8c155998749b49126e0eb80267e9b426c58677a164', // v1.8.1
