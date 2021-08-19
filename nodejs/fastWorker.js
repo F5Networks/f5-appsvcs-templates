f9bd21b14440d1fee13a314472af027c74701e07
Joel Keener <j.keener@f5.com>
2021-08-18 17:31:08 -0700

    Reuse telemetry template in dns

--- nodejs/fastWorker.js
+++ nodejs/fastWorker.js
@@ -86,7 +86,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
-        '5169073c74563637c51ca8d98767f783071d9293b453f22e5dd06fc4f21a6ff9', // 1.11
+        '751538ec143508fa10fffd7a25709707e7a4c6eb86c9fa08af5ad191ebe45218', // 1.11
         '64d9692bdab5f1e2ba835700df4d719662b9976b9ff094fe7879f74d411fe00b', // 1.10
         '89f6d8fb68435c93748de3f175f208714dcbd75de37d9286a923656971c939f0', // v1.9
         'fbaee3fd9ecce14a2d90df8c155998749b49126e0eb80267e9b426c58677a164', // v1.8.1
