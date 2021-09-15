425018cf37579ca5639a19a4893b39641033be72
Joel Keener <j.keener@f5.com>
2021-09-15 11:54:35 -0700

    Update dns mocha test for WAF_Policy

--- test/templates/dns.js
+++ test/templates/dns.js
@@ -67,7 +67,11 @@ const view = {
     // firewall
     enable_firewall: true,
     firewall_allow_list: ['10.0.0.0/8', '11.0.0.0/8'],
-    log_profile_names: ['log local']
+
+    // asm
+    enable_waf_policy: true,
+    enable_asm_logging: true,
+    asm_log_profile_names: ['log local']
 };
 
 const expected = {
@@ -102,6 +106,9 @@ const expected = {
                 policyFirewallEnforced: {
                     use: 'app1_fw_policy'
                 },
+                policyWAF: {
+                    use: 'app1_waf_policy'
+                },
                 securityLogProfiles: [
                     {
                         bigip: 'log local'
@@ -129,6 +136,9 @@ const expected = {
                 },
                 policyFirewallEnforced: {
                     use: 'app1_fw_policy'
+                },
+                policyWAF: {
+                    use: 'app1_waf_policy'
                 },
                 securityLogProfiles: [
                     {
@@ -244,6 +254,13 @@ const expected = {
                         use: 'app1_fw_rules'
                     }
                 ]
+            },
+            app1_waf_policy: {
+                class: 'WAF_Policy',
+                policy: {
+                    text: '{ "policy": { "encoding": "utf-8", "template": { "name": "POLICY_TEMPLATE_RAPID_DEPLOYMENT" } } }'
+                },
+                ignoreChanges: true
             }
         }
     }
