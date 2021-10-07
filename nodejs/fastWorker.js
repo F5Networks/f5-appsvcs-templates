0b398f227880159061a62c02ca0548b2712ddb37
Joel Keener <j.keener@f5.com>
2021-10-07 12:08:50 -0700

    FASTWorker hash update

--- nodejs/fastWorker.js
+++ nodejs/fastWorker.js
@@ -86,7 +86,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
-        'd8b16674a0bc5bdcfb97b02c60a0023ee5c4705a153ffbc7e0cecb5ca865a112', // v1.13
+        '5f28a256c377ee26be42792e7644fb1dca93266c63741a63ad49228e46a89198', // v1.13
         '42bd34feb4a63060df71c19bc4c23f9ec584507d4d3868ad75db51af8b449437', // v1.12
         '84904385ccc31f336b240ba1caa17dfab134d08efed7766fbcaea4eb61dae463', // v1.11
         '64d9692bdab5f1e2ba835700df4d719662b9976b9ff094fe7879f74d411fe00b', // v1.10
