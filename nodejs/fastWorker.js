a120d352eca3654e9d377a977f0c0bbb4151750c
Joel Keener <j.keener@f5.com>
2021-12-07 11:38:49 -0800

    Correct existing green pool verbiage

--- nodejs/fastWorker.js
+++ nodejs/fastWorker.js
@@ -70,7 +70,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
-        '1a10371beafa78a967ded9d3cb66707f263417ceea2fa9a610aacdde25bd2211', // v1.14
+        'f49849c06dd7708d647ea33b32becca2965ec9b5de9fd5dc30799bfdda62faf7', // v1.14
         '55e71bb2a511a1399bc41e9e34e657b2c0de447261ce3a1b92927094d988621e', // v1.13
         '42bd34feb4a63060df71c19bc4c23f9ec584507d4d3868ad75db51af8b449437', // v1.12
         '84904385ccc31f336b240ba1caa17dfab134d08efed7766fbcaea4eb61dae463', // v1.11
