d903e17c98b9bba04575cc6e60e9251c20b669ed
Joel Keener <j.keener@f5.com>
2022-03-28 19:53:04 +0000

    MYSTIQUE-600 HTTP template monitor dependencies issues

--- nodejs/fastWorker.js
+++ nodejs/fastWorker.js
@@ -71,7 +71,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
-        '97ef38015ded5b24ee0cdffd0f0ffe1ff8cd4d69b050251b01d77b6d65540f53', // v1.17
+        '071b908a49acde144ef02af1549fc620d7c7856c7abb49b0bfab7ad2bd94a549', // v1.17
         '0acc8d8b76793c30e847257b85e30df708341c7fb347be25bb745a63ad411cc4', // v1.16
         '5d7e87d1dafc52d230538885e96db4babe43824f06a0e808a9c401105b912aaf', // v1.15
         '5d7e87d1dafc52d230538885e96db4babe43824f06a0e808a9c401105b912aaf', // v1.14
