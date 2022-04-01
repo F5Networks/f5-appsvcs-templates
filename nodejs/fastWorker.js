a209e1ba19e868ecf6252607826314d8aebed21f
Joel Keener <j.keener@f5.com>
2022-03-31 21:00:44 -0700

    remove shape references in asm templates

--- nodejs/fastWorker.js
+++ nodejs/fastWorker.js
@@ -71,7 +71,7 @@ const configKey = 'config';
 // Known good hashes for template sets
 const supportedHashes = {
     'bigip-fast-templates': [
-        'fdd7bbe376bd199c0cd0898ae773008538fbeb1083312a5d047ce9b31e4d217c', // v1.17
+        '0164bc45aa3597ab0c93406ad206f7dce42597899b8d533296dfa335d051181f', // v1.17
         '0acc8d8b76793c30e847257b85e30df708341c7fb347be25bb745a63ad411cc4', // v1.16
         '5d7e87d1dafc52d230538885e96db4babe43824f06a0e808a9c401105b912aaf', // v1.15
         '5d7e87d1dafc52d230538885e96db4babe43824f06a0e808a9c401105b912aaf', // v1.14
