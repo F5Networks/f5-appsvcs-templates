ecba47b31c65d8db3a86d3a1be159abe68c12850
Andrey KASHCHEEV <a.kashcheev@f5.com>
2022-04-21 16:03:33 -0700

    update stand-alone/expressAdapter to expose all methods

--- stand-alone/expressAdapter.js
+++ stand-alone/expressAdapter.js
@@ -276,5 +276,8 @@ function startHttpsServer(app, options) {
 
 module.exports = {
     generateApp,
-    startHttpsServer
+    startHttpsServer,
+    restOpFromRequest,
+    setResponseFromRestOp,
+    getWorkerResponse
 };
