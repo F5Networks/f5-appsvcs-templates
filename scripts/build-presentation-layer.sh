597e5b604e41b68fd2b0c21a50f77888ac75c05b
Mitchell Stokes <m.stokes@f5.com>
2023-01-18 10:17:20 -0800

    Fix lint and redoc-cli warnings

--- scripts/build-presentation-layer.sh
+++ scripts/build-presentation-layer.sh
@@ -4,7 +4,7 @@ set -eux
 # Generate API docs
 cp docs/openapi.yml openapi-tmp.yml
 sed -i'.bu' "s%http://localhost:8100/mgmt/shared/fast%/mgmt/shared/fast%" openapi-tmp.yml
-npx redoc-cli bundle -o presentation/apidoc.html openapi-tmp.yml
+npx redoc-cli build -o presentation/apidoc.html openapi-tmp.yml
 rm openapi-tmp.*
 
 # Bundle Node modules
